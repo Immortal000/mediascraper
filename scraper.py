edd1e6a50d29a0e9e2f4387ccc38369b48ab83b8
Immortal000 <54898201+Immortal000@users.noreply.github.com>
2019-09-04 13:08:56 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,6 +1,15 @@
 from bs4 import BeautifulSoup 
 import requests 
 from bs4 import lxml
+
+'''Format: 
+Song name: 
+Song artist: 
+Release date: 
+Song description, background information: 
+Music video link: 
+Lyrics(optional): ''' 
+
 #All the websites used: 
 websites = ['https://www.genius.com', 'https://www.shazam.com', 'https://www.musicbrainz.org']
 song = input('Enter the songs name:')
