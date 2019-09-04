ed659f4d442a5cbb1812b97d4bcbb82947269919
Immortal000 <54898201+Immortal000@users.noreply.github.com>
2019-09-04 13:00:10 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,4 +1,10 @@
 from bs4 import BeautifulSoup 
+import requests 
+from bs4 import lxml
 #All the websites used: 
 websites = ['https://www.genius.com', 'https://www.shazam.com', 'https://www.musicbrainz.org']
 song = input('Enter the songs name:')
+
+#Extracting the music vieo 
+website = requests.get('https://google.com/search?q=%s'+song+'song')
+soup = BeautifulSoup(website, 'lxml')
