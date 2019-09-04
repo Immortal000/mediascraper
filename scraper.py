808e8ceaa7c0c63434d13a19fb3afecfbdf06f06
Immortal000 <54898201+Immortal000@users.noreply.github.com>
2019-09-04 09:00:31 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,1 +1,4 @@
 from bs4 import BeautifulSoup 
+#All the websites used: 
+websites = ['https://www.genius.com', 'https://www.shazam.com', 'https://www.musicrainz.org']
+song = input('Enter the songs name:')
