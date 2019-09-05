f1dd4aa7ce9efaaf0756c02e6858b0e0fa56f1ee
Mithun <sendmithunb@gmail.com>
2019-09-04 23:20:12 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -9,21 +9,21 @@ Song description, background information:
 Music video link:
 Lyrics(optional):
 '''
-song = input('Enter the songs name:') 
-#Gets the website 
+song = input('Enter the songs name:')
+#Gets the website
 website = requests.get('https://www.lyricsmode.com/search.php?search='+song).text
 soup = BeautifulSoup(website,'lxml')
-#Finds for the links of the song 
+#Finds for the links of the song
 for a in soup.find_all('a', href=True):
     links.append(a['href'])
 #Gets the weblink of the song lyric
 song_website = requests.get('https://www.lyricsmode.com'+links[12]).text
 song_soup = BeautifulSoup(song_website,'lxml')
 #Prints the lyrics
-try: 
+try:
     print(song_soup.find('div', id = "lyrics_text").text)
 #Scenario if any error is raised
-except: 
+except:
     print('Song not found in the database')
 #file digestion
 def file_digest():
@@ -34,5 +34,5 @@ def file_digest():
         Placeholder for the file input function using f.
         '''
         fileQ -= 1
-
+#as
 file_digest()
