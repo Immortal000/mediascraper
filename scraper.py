8c565db736ade3cab53397369a422453de0b8f9a
Mithun <sendmithunb@gmail.com>
2019-09-04 20:36:25 -0500

    Update scraper.py
    Added placeholders for the digest functions

--- scraper.py
+++ scraper.py
@@ -10,6 +10,7 @@ Release date:
 Song description, background information:
 Music video link:
 Lyrics(optional):
+'''
 
 #All the websites used:
 websites = ['https://www.genius.com', 'https://www.shazam.com', 'https://www.musicbrainz.org']
@@ -18,13 +19,16 @@ song = input('Enter the songs name:')
 #Extracting the music video
 website = requests.get('https://google.com/search?q=%s'+song+'song')
 soup = BeautifulSoup(website, 'lxml')
-'''
 
-file_digest()
+
 #file digestion
 def file_digest():
-    fileQ = 1:
-    print(fileQ)
-    while fileQ =! 0:
-        print("test")
-        fileQ += 1
+    fileQ = 5 #placeholder value
+    #fileQ is the number of files
+    while fileQ != 0:
+        '''
+        Placeholder for the file input function using f.
+        '''
+        fileQ -= 1
+
+file_digest()
