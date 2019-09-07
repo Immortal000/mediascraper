a5708e347ef719ce0458671daaf99a798e0cb984
Immortal000 <54898201+Immortal000@users.noreply.github.com>
2019-09-06 23:08:55 -0500

    Update ingest.py

--- ingest.py
+++ ingest.py
@@ -1,8 +1,8 @@
-import eyed3 as eyed3
+import eyed3 
 import magic
 
 audiofile = eyed3.load("test-mp3.mp3")
-audiofile.initTag() 
+audiofile.initTag()
 audiofile.tag.artist = u"Integrity"
 audiofile.tag.album = u"Humanity Is The Devil"
 audiofile.tag.album_artist = u"Integrity"
