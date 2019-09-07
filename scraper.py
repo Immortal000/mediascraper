9d2e497479a4367c25a6daf82a2be10394a7bfa5
Immortal000 <54898201+Immortal000@users.noreply.github.com>
2019-09-07 17:25:32 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -5,16 +5,6 @@ import requests               #
 import re                     #
 ###############################
 links = []
-
-'''
-Format:
-Song name:
-Song artist:
-Release date:
-Song description, background information:
-Music video link:
-Lyrics(optional):
-'''
 song = input('Enter the songs name:')
 #Gets the website
 website = requests.get('https://www.lyricsmode.com/search.php?search='+song).text
@@ -27,19 +17,21 @@ song_website = requests.get('https://www.lyricsmode.com'+links[12]).text
 song_soup = BeautifulSoup(song_website,'lxml')
 #Prints the lyrics
 try:
-    yt_links = []
     lyrics = song_soup.find('div', id = "lyrics_text").text
-    #DONOT EDIT --- print(f'Song name:{Song}\nSong artist: {}')
     artist_name = song_soup.find('span', attrs={'class':'fs32'}).text
-    yt_search = requests.get('https://www.google.com/search?q=youtube+link'+song+artist_name).text
-    yt_soup = BeautifulSoup(yt_search, 'lxml')
-    for l in soup.find_all('a', href=True):
-        links.append(l['href'])
-    #print(f"Song Name|{song}\nYoutube Link|{yt_links[146]}\nLyrics:\n{lyrics}")
-        
+    print(f"Song Name|{song}\nLyrics:\n{lyrics}")        
 #Scenario if any error is raised
 except:
     print('Song not found in the database')
+
+#Searching for youtube Link
+
+yt_search = requests.get('https://www.youtube.com/results?search_query='+song+artist_name,'youtube+link').text
+yt_soup = BeautifulSoup(yt_search, 'html.parser')
+a = yt_soup.find_all("a", href=True)
+for i in a:
+    url = i.get('href')
+    print(f'\t\t{url}')
 #file digestion
 
 def file_digest():
