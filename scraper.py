7fde1291ff9ab1d3eb26f49293ecd9523ae9c061
Mithun <sendmithunb@gmail.com>
2019-09-06 22:51:59 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,9 +1,9 @@
-
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup 
 import requests
 import re
 
 links = []
+
 '''
 Format:
 Song name:
@@ -21,16 +21,19 @@ soup = BeautifulSoup(website,'lxml')
 #Finds for the links of the song
 for a in soup.find_all('a', href=True):
     links.append(a['href'])
-'''for link in links:
-    print(link)'''
 #Gets the weblink of the song lyric
 song_website = requests.get('https://www.lyricsmode.com'+links[12]).text
 song_soup = BeautifulSoup(song_website,'lxml')
 #Prints the lyrics
 try:
     lyrics = song_soup.find('div', id = "lyrics_text").text
-    print(lyrics)
     #DONOT EDIT --- print(f'Song name:{Song}\nSong artist: {}')
+    artist_name = song_soup.find('span', attrs={'class':'fs32'}).text
+    print(artist_name,lyrics)
+    google_search = requests.get('https://www.google.com/search?q='+song,artist_name)
+    google_soup = BeautifulSoup(google_search, 'lxml')
+    yt_link = google_search.find('cite', attrs={'class':'iUh30 bc'}).text
+
 #Scenario if any error is raised
 except:
     print('Song not found in the database')
@@ -42,3 +45,7 @@ def file_digest():
     while fileQ != 0:
         fileQ -= 1
 file_digest()
+
+#Working on the formatting
+
+#print(f"Song Name| {song_name}\nRelease Date| {release_date}\nYoutube Link|{yt_link}\nLyrics:\n{lyrics}")
