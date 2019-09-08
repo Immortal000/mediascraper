e8ee546da0d184aa286c516dee91fd71fbc08063
Immortal000 <54898201+Immortal000@users.noreply.github.com>
2019-09-07 20:08:50 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -4,34 +4,39 @@ from bs4 import BeautifulSoup #
 import requests               #
 import re                     #         
 ###############################
-links = []
-song = input('Enter the songs name:')
-#Gets the website
-website = requests.get('https://www.lyricsmode.com/search.php?search='+song).text
-soup = BeautifulSoup(website,'lxml')
-#Finds for the links of the song
-for a in soup.find_all('a', href=True):
-    links.append(a['href'])
-#Gets the weblink of the song lyric
-song_website = requests.get('https://www.lyricsmode.com'+links[12]).text
-song_soup = BeautifulSoup(song_website,'lxml')
-#Prints the lyrics
-try:
-    lyrics = song_soup.find('div', id = "lyrics_text").text
-    artist_name = song_soup.find('span', attrs={'class':'fs32'}).text
-    print(f"Song Name|{song}\nLyrics:\n{lyrics}")        
-#Scenario if any error is raised
-except:
-    print('Song not found in the database')
+global song, artist_name, index
+index = 12 
+song = input('Song:')
+def get_lyrics(song, index):
+     links = []
+     website = requests.get('https://www.lyricsmode.com/search.php?search='+song).text
+     soup = BeautifulSoup(website,'lxml')
+     for a in soup.find_all('a', href=True):
+         links.append(a['href'])
+     song_website = requests.get('https://www.lyricsmode.com'+links[index]).text
+     song_soup = BeautifulSoup(song_website,'lxml')
+     
+     lyrics = song_soup.find('div', id = "lyrics_text").text
+     artist_name = song_soup.find('span', attrs={'class':'fs32'}).text
+     print(f"Song Name|{song}\nArtist Name|{artist_name}\nLyrics:\n{lyrics}")     
+     '''except: 
+         print('Song not found in database')'''
+     answer = input('Is this the right song?')
+     if answer == 'no':
+          index = index + 2
+          get_lyrics(song,index)
+     else:
+          continue
+get_lyrics(song,index)
 
 #Searching for youtube Link
-yt_links = []
+'''yt_links = []
 yt_search = requests.get('https://www.bing.com/videos/search?q='+song+artist_name,'youtube+link').text
 yt_soup = BeautifulSoup(yt_search, 'html.parser')
 a = yt_soup.findAll("div",{"class":"mc_vtvc"})
 for i in a:
     print(i)
-'''for i in a:
+for i in a:
     yt_links.append(i.get('href'))
 print(yt_links)''' 
 
