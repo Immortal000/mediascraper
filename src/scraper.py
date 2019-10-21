449acbe2b9803f48b603efa7f2be60150c8f5aa4
Immortal000 <54898201+Immortal000@users.noreply.github.com>
2019-10-20 22:38:56 -0500

    Create scraper.py

---
+++ src/scraper.py
@@ -0,0 +1,55 @@
+
+###############################
+from bs4 import BeautifulSoup #
+import requests               #
+import re                     #                
+###############################
+global song, artist_name, index
+index = 12 
+song = input('Song:')
+def get_youtube_link(song,artist_name):
+     yt_links = []
+     website = requests.get('https://www.youtube.com/results?search_query='+song+artist_name+'video+song').text
+     soup = BeautifulSoup(website, 'lxml')
+     for link in soup.findAll(attrs={'class':'yt-uix-tile-link'}):
+          yt_links.append('https://www.youtube.com'+link['href'])
+     print(yt_links[0])
+
+     
+def get_lyrics(song, index):
+     links = []
+     website = requests.get('https://www.lyricsmode.com/search.php?search='+song).text
+     soup = BeautifulSoup(website,'lxml')
+     for a in soup.find_all('a', href=True):
+         links.append(a['href'])
+     song_website = requests.get('https://www.lyricsmode.com'+links[index]).text
+     song_soup = BeautifulSoup(song_website,'lxml')
+     try: 
+          lyrics = song_soup.find('div', id = "lyrics_text").text
+          artist_name = song_soup.find('span', attrs={'class':'fs32'}).text
+          print(f"Song Name|{song}\nArtist Name|{artist_name}\nLyrics:\n{lyrics}")     
+     except: 
+         print('Song not found in database')
+     answer = input('Is this the right song?')
+     if answer == 'no':
+          index = index + 2
+          get_lyrics(song,index)
+     else:
+          get_youtube_link(song,artist_name)
+get_lyrics(song,index)
+
+
+#file digestion
+
+def file_digest():
+    fileQ = 5 #placeholder value
+    #fileQ is the number of files
+    while fileQ != 0:
+        '''
+        Placeholder for the file input function using f.
+        '''
+        fileQ -= 1
+file_digest()
+
+#Working on the formatting 
+
